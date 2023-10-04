8a31c9da694b90ec217a9787d738061d0e51a01d
Vladislav <v1.masalsky@yandex.ru>
2023-10-04 22:02:06 +0300

    get clients+

---
+++ public/commands/SyncController.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\commands;
+
+use app\actions\console\GetClientsAction;
+use yii\console\Controller;
+
+/**
+ * Main sync controller
+ */
+class SyncController extends Controller
+{
+    /**
+     * @inheritdoc
+     *
+     * @return string[]
+     */
+    public function actions(): array
+    {
+        return [
+            'clients' => GetClientsAction::class
+        ];
+    }
+}
