fd9b8a9738ec110dfecb173a3d804989b4879f8b
Vladislav <v1.masalsky@yandex.ru>
2023-10-06 19:50:41 +0300

    fix

--- public/controllers/SiteController.php
+++ public/controllers/SiteController.php
@@ -6,7 +6,6 @@ namespace app\controllers;
 
 use app\services\AggregatorService;
 use yii\base\Response;
-use yii\redis\Cache;
 use yii\rest\Controller;
 
 class SiteController extends Controller
@@ -15,14 +14,12 @@ class SiteController extends Controller
      * @param $id
      * @param $module
      * @param AggregatorService $aggregatorService
-     * @param Cache $cache
      * @param array $config
      */
     public function __construct(
         $id,
         $module,
         private AggregatorService $aggregatorService,
-        private Cache $cache,
         array $config = []
     ) {
         parent::__construct($id, $module, $config);
@@ -35,7 +32,6 @@ class SiteController extends Controller
      */
     public function actionIndex(): Response
     {
-        $this->cache->set('test', 123);
         return $this->asJson($this->aggregatorService->log($this->aggregatorService->getIncomingData()));
     }
 }
